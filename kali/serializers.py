94182021e557de893a156dee2319368c8e1dcf11
mxbialostocki <matt.bialostocki@gmail.com>
2021-03-10 21:25:55 +1300

    Add initial Serializer

---
+++ kali/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Book
+
+"""
+Serilaization: Transforming an object into another data format.
+Once it's transformed it can be sent through a Network.
+A Django Model is a Python Class.
+How do you render a Python Class to a browser?
+With a REST Serializer.
+It works the other way around too: Converts JSON to a objects in Python.
+
+This means you can:
+ - display the Django objects in a browser by converting them to JSON
+ - make CRUD requests with a JSON payload to an API
+"""
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'author')
