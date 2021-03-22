cb0e8a46b612f4dd0546d988e44b3ef59785eddc
mxbialostocki <matt.bialostocki@gmail.com>
2021-03-22 21:12:42 +1300

    add theme to codebase

---
+++ frontend/src/theme/custom.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  homePage: {
+    width: '100vw',
+    height: '100vh',
+    justifyContent: 'center',
+    alignContent: 'center',
+    [theme.breakpoints.down('sm')]: {
+      justifyContent: 'flex-start'
+    }
+  },
+  colorTest: {
+      color: "red"
+  }
+}))
+
+export default useStyles
